e5c0cc86ec27123b5f87dbfe8206ff8dd8f0a07e
adilsontchameia <adilsontchameia@gmail.com>
2023-10-02 00:20:09 +0100

    feat: imports

--- lib/presentation/providers/providers.dart
+++ lib/presentation/providers/providers.dart
@@ -6,3 +6,5 @@ export 'package:flutter_riverpod/flutter_riverpod.dart';
 
 export 'movies/movies_slideshow_provider.dart';
 export 'movies/initial_loading_provider.dart';
+export 'actors/actors_by_movie_provider.dart';
+export 'actors/actors_repository_provider.dart';
