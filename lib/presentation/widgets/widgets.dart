c7d89fbd687b508823af72d03acfeb02d8654f80
adilsontchameia <adilsontchameia@gmail.com>
2023-10-06 00:20:11 +0100

    feat: imports

--- lib/presentation/widgets/widgets.dart
+++ lib/presentation/widgets/widgets.dart
@@ -1,5 +1,7 @@
 export 'shared/custom_app.dart';
-export 'movies/movies_slideshow.dart';
+export 'home_screen/movies_slideshow.dart';
 export 'shared/custom_bottom_navigation.dart';
-export 'movies/movie_horizontal_listview.dart';
+export 'home_screen/movie_horizontal_listview.dart';
 export 'shared/full_screen_loader.dart';
+export 'movie_details/custom_gradient.dart';
+export 'movie_details/custom_sliver_appbar.dart';
