1cd966d5464d847257e0fdecffb4ee0363259ea4
adilsontchameia <adilsontchameia@gmail.com>
2023-12-03 12:25:09 +0100

    feat: routes config

---
+++ lib/config/router/app_router.dart
@@ -0,0 +1,11 @@
+import 'package:go_router/go_router.dart';
+
+import '../../presentation/screens/screens.dart';
+
+final appRouter = GoRouter(initialLocation: '/', routes: [
+  GoRoute(
+    path: '/',
+    name: HomeScreen.name,
+    builder: (context, state) => const HomeScreen(),
+  )
+]);
