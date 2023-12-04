cb83e67fe8255045b35c2afb9a662e8386d49a55
adilsontchameia <adilsontchameia@gmail.com>
2023-12-04 20:42:54 +0100

    feat: avoid many requests at one time

--- lib/presentation/providers/movies/movies_provider.dart
+++ lib/presentation/providers/movies/movies_provider.dart
@@ -12,15 +12,24 @@ typedef MovieCallback = Future<List<Movie>> Function({int page});
 
 class MoviesNotifier extends StateNotifier<List<Movie>> {
   int currentPage = 0;
+  bool isLoading = false;
   MovieCallback fetchMoreMovies;
 
   MoviesNotifier({required this.fetchMoreMovies}) : super([]);
 
   Future<void> loadNextPage() async {
-    currentPage++;
+    if (isLoading) return;
+    isLoading = true;
 
+    currentPage++;
     //give new state (value) it will now that has a change
     final List<Movie> movies = await fetchMoreMovies(page: currentPage);
     state = [...state, ...movies];
+
+    await delayHttpRequest();
+    isLoading = false;
   }
+
+  Future<dynamic> delayHttpRequest() =>
+      Future.delayed(const Duration(milliseconds: 300));
 }
